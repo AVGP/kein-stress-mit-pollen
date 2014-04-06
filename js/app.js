e288db1de53d09b3db9c8a2b2c5b380a39e2e11b
Martin N <mr.avgp@googlemail.com>
2014-04-06 12:28:46 -0400

    clean slate...

--- js/app.js
+++ js/app.js
@@ -1,14 +1,69 @@
-var pollen = angular.module('pollen', []);
+// Ionic Starter App
 
-function AppCtrl($scope, $http) {
-    $scope.TREE_SPECS = {
-        Betula:   { name: "Birke", color: '#f03', baseRadius: 30 },
-        Alnus:    { name: "Erle",  color: '#30f', baseRadius: 15 },
-        Corylus:  { name: "Hasel", color: '#3f0', baseRadius: 15 },
-        Fraxinus: { name: "Esche", color: '#83c', baseRadius: 15 },
-        Fagus:    { name: "Buche", color: '#3cf', baseRadius:  7 },
-        Quercus:  { name: "Eiche", color: '#c3f', baseRadius:  7 },
-        Salix:    { name: "Weide", color: '#aa6', baseRadius:  1 }
-      };
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-}
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    // setup an abstract state for the tabs directive
+    .state('tab', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+    })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.map', {
+      url: '/map',
+      views: {
+        'tab-map': {
+          templateUrl: 'templates/tab-map.html',
+          controller: 'MapCtrl'
+        }
+      }
+    })
+
+    .state('tab.prognosis', {
+      url: '/prognosis',
+      views: {
+        'tab-prognosis': {
+          templateUrl: 'templates/tab-prognosis.html',
+          controller: 'PrognosisCtrl'
+        }
+      }
+    })
+    .state('tab.settings', {
+      url: '/settings',
+      views: {
+        'tab-settings': {
+          templateUrl: 'templates/tab-settings.html',
+          controller: 'SettingsCtrl'
+        }
+      }
+    })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/map');
+
+});
+
